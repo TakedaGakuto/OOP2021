9f227a67c7b0a7f8d3a37e957b315045781f29cf
GakutoTakeda <32042@ojs.ac.jp>
2021-05-25 10:19:13 +0900

    P52 リスト2.6 変換クラス作成

---
+++ DistanceConverter/DistanceConverter/FeetConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistanceConverter {
+    public class FeetConverter {
+        public double ToMeter(double feet) {
+            return feet * 0.3048;
+        }
+        public double FromMeter(double meter) {
+            return meter / 0.3048;
+        }
+    }
+}
--- DistanceConverter/DistanceConverter/Program.cs
+++ DistanceConverter/DistanceConverter/Program.cs
@@ -16,23 +16,20 @@ namespace DistanceConverter {
         }
         //フィートからメートル
         private static void PrintFeetToMeterList(int start, int stop) {
+            FeetConverter converter = new FeetConverter();
             for (int feet = 1; feet <= 10; feet++) {
-                double meter = FeetToMeter(feet);
+                double meter = converter.ToMeter(feet);
                 Console.WriteLine("{0} ft={1:0.0000}m", feet, meter);
             }
         }
         //メートルからフィート
         private static void PrintMeterToFeetList(int start,int stop) {
+            FeetConverter converter = new FeetConverter();
             for (int meter = 1; meter <= 10; meter++) {
-                double feet = MeterToFeet(meter);
+                double feet = converter.FromMeter(meter);
                 Console.WriteLine("{0} m={1:0.0000}ft", meter, feet);
             }
         }
-        static double FeetToMeter(int feet) {
-            return feet * 0.3048;
-        }
-        static double MeterToFeet(int meter) {
-            return meter / 0.3048;
-        }
+        
     }
 }
