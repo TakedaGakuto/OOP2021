491834e16f37c7afe0df4189f597b310e13fb7a6
GakutoTakeda <32042@ojs.ac.jp>
2021-05-25 11:03:08 +0900

    P55リスト2.10 定数を定義

--- DistanceConverter/DistanceConverter/FeetConverter.cs
+++ DistanceConverter/DistanceConverter/FeetConverter.cs
@@ -5,12 +5,13 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace DistanceConverter {
-    public static class FeetConverter {  //静的クラスにすることで、インスタンス化(new)したときにエラーが出るように
+    public static class FeetConverter {  //静的クラスにすることで、インスタンス化
+        private const double ratio = 0.3048;  //publicのときは、readonlyを使う
         public static double ToMeter(double feet) {
-            return feet * 0.3048;
+            return feet * ratio;
         }
         public static double FromMeter(double meter) {
-            return meter / 0.3048;
+            return meter / ratio;
         }
     }
 }
