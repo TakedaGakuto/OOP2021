9d0ff806eb30e2e6a0f77f29bb82ef2f08e8d7cd
GakutoTakeda <32042@ojs.ac.jp>
2021-05-28 14:05:45 +0900

    リスト2.27 完了

--- SalesCalculator/SalesCounter.cs
+++ SalesCalculator/SalesCounter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,16 +9,32 @@ namespace SalesCalculator {
     //売り上げ集計クラス
     class SalesCounter {
 
-        private List<Sale> _sales; //「_」を付ける理由：インテリセンスで出しやすくするため
+        private IEnumerable<Sale> _sales; //「_」を付ける理由：インテリセンスで出しやすくするため
 
-        public SalesCounter(List<Sale> sales) {
-            _sales = sales;
+        public SalesCounter(string filePath) {
+            _sales = ReadSales(filePath);
+        }
+
+        //データを読み取りリストを返す
+         private static IEnumerable<Sale> ReadSales(string filePath) {
+            List<Sale> sales = new List<Sale>();
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines) {
+                string[] items = line.Split(',');　//「,」で3つに区切って、それぞれ配列に
+                Sale sale = new Sale {
+                    ShopName = items[0],
+                    ProductCategory = items[1],
+                    Amount = int.Parse(items[2])
+                };//オブジェクト初期化子...オブジェクト初期化子を使うことで、メンテナンス性が良
+                sales.Add(sale);
+            }
+            return sales;
         }
 
         //店舗別売り上げ集計メソッド
-        public Dictionary<string,int> GetPerStoreSales() {
-            Dictionary<string, int> dict = new Dictionary<string, int>();
-            foreach(Sale sale in _sales) {
+        public IDictionary<string,int> GetPerStoreSales() {
+            var dict = new Dictionary<string, int>();
+            foreach(var sale in _sales) {
                 //既存の場合
                 if (dict.ContainsKey(sale.ShopName))
                     dict[sale.ShopName] += sale.Amount;
