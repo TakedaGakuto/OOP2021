2348506f32be642ea01f7d658ec6c1336ee38857
GakutoTakeda <32042@ojs.ac.jp>
2021-06-01 11:25:14 +0900

    完成

--- DistanceConverter/Exarcise3/Program.cs
+++ DistanceConverter/Exarcise3/Program.cs
@@ -11,13 +11,13 @@ namespace Exarcise3 {
             //List<Sale> sales = ReadSales("Sales.csv");　//ドライバ(テスト用上位モジュール)↔スタブ
 
             var sales = new SalesCounter("sales.csv");//SalesCounterにReadSalesの結果を渡す
-
+            //店舗別
             IDictionary<string, int> amounPerStore = sales.GetPerStoreSales();
             foreach (KeyValuePair<string,int> obj in amounPerStore) {
                 Console.WriteLine("{0} : {1:#,0}円", obj.Key, obj.Value);
-                
             }
             Console.WriteLine("");
+            //商品カテゴリ別
             IDictionary<string, int> amounPerProduct = sales.GetPerProductSales();
             foreach (KeyValuePair<string, int> obj in amounPerProduct) {
                 Console.WriteLine("{0} : {1:#,0}円", obj.Key, obj.Value);
