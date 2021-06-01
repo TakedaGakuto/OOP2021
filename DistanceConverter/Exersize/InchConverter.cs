4c1e371ce785c8080dbc745b521d25b205cb6b32
GakutoTakeda <32042@ojs.ac.jp>
2021-06-01 10:20:32 +0900

    問題2.2完了

---
+++ DistanceConverter/Exersize/InchConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exersize {
+    class InchConverter {
+        private const double ratio = 0.0254;  //publicのときは、readonlyを使う
+        public static double ToInch(double inch) {
+            return inch * ratio;
+        }
+        public static double FromInch(double meter) {
+            return meter * ratio;
+        }
+    }
+}
--- DistanceConverter/Exersize/Program.cs
+++ DistanceConverter/Exersize/Program.cs
@@ -7,7 +7,28 @@ using System.Threading.Tasks;
 namespace Exersize {
     class Program {
         static void Main(string[] args) {
-            
+            if (args.Length >= 1 && args[0] == "-toi") {
+                //メートルからインチ
+                PrintMeterToInchList(1, 10);
+            }
+            else {
+                //インチからメートル
+                PrintInchToMeterList(1, 10);
+            }
+
+        }
+       
+        private static void PrintInchToMeterList(int start, int stop) {
+            for (int meter = 1; meter <= 10; meter++) {
+                double inch = InchConverter.FromInch(meter);
+                Console.WriteLine("{0} m={1:0.0000}inch", meter, inch);
+            }
+        }
+        private static void PrintMeterToInchList(int start, int stop) {
+            for (int inch = 1; inch <= 10; inch++) {
+                double meter = InchConverter.ToInch(inch);
+                Console.WriteLine("{0} inch={1:0.0000}m", inch, meter);
+            }
         }
     }
 }
