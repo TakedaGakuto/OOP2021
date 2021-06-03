fe203cf2bcd4d6accc14c9a269c664453e9f0dbd
GakutoTakeda <32042@ojs.ac.jp>
2021-06-03 14:02:00 +0900

    P87まで

--- Section01/Program.cs
+++ Section01/Program.cs
@@ -8,19 +8,22 @@ namespace Section01 {
     class Program {
         static void Main(string[] args) {
             var sc = new SampleCode();
-            Console.WriteLine(sc.Count(5));
+            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
+            //var count = sc.Count(numbers, delegate(int n) { return n % 2 == 0; });
+            var count = sc.Count(numbers,n => n. ToString().Contains('1'));
+            Console.WriteLine(count);
         }
-        class SampleCode {
-            public int Count(int num) {
-                var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
-                int count = 0;
-                foreach (var n in numbers) {
-                    if (n == num) {
-                        count++;
-                    }
+    }
+       
+    class SampleCode {
+        public int Count(int[] numbers,Predicate<int> judge) {
+            int count = 0;
+            foreach (var n in numbers) {
+                if (judge(n) ==true) {
+                    count++;
                 }
-                return count;
             }
+            return count;
         }
     }
 }
