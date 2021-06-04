9d1d95963cf4e4f0d1cdd9c1c13e9758621d40c5
GakutoTakeda <32042@ojs.ac.jp>
2021-06-04 15:55:07 +0900

    追加問題②途中

--- DistanceConverter/Exarcise3-3/Program.cs
+++ DistanceConverter/Exarcise3-3/Program.cs
@@ -9,8 +9,27 @@ namespace Exarcise3_3 {
         static void Main(string[] args) {
             var names = new List<string> {
                  "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong",};
-           //小文字カウント
-           
+            var words = new List<string> { };
+            // 3.3.1
+            Exercise3_1(names);
+            Console.WriteLine("-----");
+            //3.3.2
+            Exarcise3_2(words);
+        }
+
+        private static void Exarcise3_2(List<string> words) {
+          
+        }
+
+        private static void Exercise3_1(List<string> names) {
+            int count1 = 0;
+            int count2 = 0;
+            foreach(var name in names) {
+                count1 += name.Count(c => char.IsLower(c));
+                count2 += name.Count(c => char.IsUpper(c));
+            }
+            Console.WriteLine(count1);
+            Console.WriteLine(count2);
         }
     }
 }
