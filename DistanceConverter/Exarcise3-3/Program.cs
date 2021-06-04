03a6616809041df8d756039a5dad0a8542aa5b22
GakutoTakeda <32042@ojs.ac.jp>
2021-06-04 15:35:06 +0900

    追加問題開始

---
+++ DistanceConverter/Exarcise3-3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exarcise3_3 {
+    class Program {
+        static void Main(string[] args) {
+            var names = new List<string> {
+                 "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong",};
+           //小文字カウント
+           
+        }
+    }
+}
