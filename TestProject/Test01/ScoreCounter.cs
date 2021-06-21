4e4e68e83c4a95a344e922b1c43b697c05f4db31
GakutoTakeda <32042@ojs.ac.jp>
2021-06-21 14:19:00 +0900

    設問１完了

--- TestProject/Test01/ScoreCounter.cs
+++ TestProject/Test01/ScoreCounter.cs
@@ -7,33 +7,38 @@ namespace Test01 {
 
         // コンストラクタ
         public ScoreCounter(string filePath) {
-
-
-
-            
+            _score = ReadScore(filePath);                        
         }
 
 
-        //メソッドの概要： 
+        //メソッドの概要：CSVファイルデータを、「,」で区切り配列に追加
         private static IEnumerable<Student> ReadScore(string filePath) {
-            
-
-
-
-
-
-
-            
+            //
+            List<Student> students = new List<Student>();
+            string[] Lines = File.ReadAllLines(filePath);
+            foreach(string line in Lines) {
+                string[] items = line.Split(',');
+                Student student = new Student {
+                    Name = items[0],
+                    Subject = items[1],
+                    Score = int.Parse(items[2])
+                };
+                students.Add(student);
+            }
+            return students;
         }
 
-        //メソッドの概要： 
+        //メソッドの概要： 科目ごとの合計を集計
         public IDictionary<string, int> GetPerStudentScore() {
-
-
-
-
-
-            
+            IDictionary<string, int> dict = new Dictionary<string, int>();
+            foreach(Student student in _score) {
+                if (dict.ContainsKey(student.Subject)) {
+                    dict[student.Subject] += student.Score;
+                }else {
+                    dict[student.Subject] = student.Score;
+                }
+            }
+            return dict;
         }
     }
 }
--- TestProject/Test01/Student.cs
+++ TestProject/Test01/Student.cs
@@ -1,7 +1,11 @@
 ﻿namespace Test01 {
     class Student {
 
-
-
+        //学生名
+        public string Name { get; set; }
+        //科目名
+        public string Subject { get; set; }
+        //点数
+        public int Score { get; set; }
     }
 }
