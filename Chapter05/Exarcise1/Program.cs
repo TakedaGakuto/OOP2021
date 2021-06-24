653cc66ad711ea11aec6591bc1f483ee2f014c29
GakutoTakeda <32042@ojs.ac.jp>
2021-06-24 13:30:43 +0900

    問題5-1終了

--- Chapter05/Exarcise1/Program.cs
+++ Chapter05/Exarcise1/Program.cs
@@ -7,20 +7,11 @@ using System.Threading.Tasks;
 namespace Exarcise1 {
     class Program {
         static void Main(string[] args) {
-
             //5.1
             var s1 = Console.ReadLine();
             var s2 = Console.ReadLine();
             Exarcise1_1(s1, s2);
-
-            //5.2
-            Exarcise1_2(s1, s2);
         }
-
-        private static void Exarcise1_2(string s1, string s2) {
-        
-        }
-
         private static void Exarcise1_1(string s1, string s2) {
             if(String.Compare(s1,s2,ignoreCase:true) == 0) {
                 Console.WriteLine("２つの文字列は等しいです。");
