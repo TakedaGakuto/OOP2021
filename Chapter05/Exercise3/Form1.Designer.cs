4449293cecf649bf2653aea6051cf23fedf89ce1
GakutoTakeda <32042@ojs.ac.jp>
2021-06-24 15:51:16 +0900

    問題5.3終了

---
+++ Chapter05/Exercise3/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿
+namespace Exercise3 {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.inputStrText = new System.Windows.Forms.TextBox();
+            this.ButtonQ1 = new System.Windows.Forms.Button();
+            this.ButtonQ2 = new System.Windows.Forms.Button();
+            this.ButtonQ3 = new System.Windows.Forms.Button();
+            this.ButtonQ5 = new System.Windows.Forms.Button();
+            this.ButtonQ4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.AnsText = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.BeforeText = new System.Windows.Forms.TextBox();
+            this.AfterText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 20F);
+            this.label1.Location = new System.Drawing.Point(33, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "問題5.3";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            // 
+            // inputStrText
+            // 
+            this.inputStrText.Font = new System.Drawing.Font("MS UI Gothic", 18F);
+            this.inputStrText.Location = new System.Drawing.Point(38, 72);
+            this.inputStrText.Name = "inputStrText";
+            this.inputStrText.Size = new System.Drawing.Size(721, 31);
+            this.inputStrText.TabIndex = 1;
+            // 
+            // ButtonQ1
+            // 
+            this.ButtonQ1.Font = new System.Drawing.Font("MS UI Gothic", 20F);
+            this.ButtonQ1.Location = new System.Drawing.Point(38, 160);
+            this.ButtonQ1.Name = "ButtonQ1";
+            this.ButtonQ1.Size = new System.Drawing.Size(71, 48);
+            this.ButtonQ1.TabIndex = 2;
+            this.ButtonQ1.Text = "問１";
+            this.ButtonQ1.UseVisualStyleBackColor = true;
+            this.ButtonQ1.Click += new System.EventHandler(this.ButtonQ1_Click);
+            // 
+            // ButtonQ2
+            // 
+            this.ButtonQ2.Font = new System.Drawing.Font("MS UI Gothic", 20F);
+            this.ButtonQ2.Location = new System.Drawing.Point(144, 160);
+            this.ButtonQ2.Name = "ButtonQ2";
+            this.ButtonQ2.Size = new System.Drawing.Size(71, 48);
+            this.ButtonQ2.TabIndex = 2;
+            this.ButtonQ2.Text = "問2";
+            this.ButtonQ2.UseVisualStyleBackColor = true;
+            this.ButtonQ2.Click += new System.EventHandler(this.ButtonQ2_Click);
+            // 
+            // ButtonQ3
+            // 
+            this.ButtonQ3.Font = new System.Drawing.Font("MS UI Gothic", 20F);
+            this.ButtonQ3.Location = new System.Drawing.Point(251, 160);
+            this.ButtonQ3.Name = "ButtonQ3";
+            this.ButtonQ3.Size = new System.Drawing.Size(71, 48);
+            this.ButtonQ3.TabIndex = 2;
+            this.ButtonQ3.Text = "問3";
+            this.ButtonQ3.UseVisualStyleBackColor = true;
+            this.ButtonQ3.Click += new System.EventHandler(this.ButtonQ3_Click);
+            // 
+            // ButtonQ5
+            // 
+            this.ButtonQ5.Font = new System.Drawing.Font("MS UI Gothic", 20F);
+            this.ButtonQ5.Location = new System.Drawing.Point(461, 160);
+            this.ButtonQ5.Name = "ButtonQ5";
+            this.ButtonQ5.Size = new System.Drawing.Size(71, 48);
+            this.ButtonQ5.TabIndex = 2;
+            this.ButtonQ5.Text = "問5";
+            this.ButtonQ5.UseVisualStyleBackColor = true;
+            this.ButtonQ5.Click += new System.EventHandler(this.ButtonQ5_Click);
+            // 
+            // ButtonQ4
+            // 
+            this.ButtonQ4.Font = new System.Drawing.Font("MS UI Gothic", 20F);
+            this.ButtonQ4.Location = new System.Drawing.Point(356, 160);
+            this.ButtonQ4.Name = "ButtonQ4";
+            this.ButtonQ4.Size = new System.Drawing.Size(71, 48);
+            this.ButtonQ4.TabIndex = 2;
+            this.ButtonQ4.Text = "問4";
+            this.ButtonQ4.UseVisualStyleBackColor = true;
+            this.ButtonQ4.Click += new System.EventHandler(this.ButtonQ4_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 20F);
+            this.label2.Location = new System.Drawing.Point(38, 252);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 27);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "実行結果";
+            // 
+            // AnsText
+            // 
+            this.AnsText.Font = new System.Drawing.Font("MS UI Gothic", 18F);
+            this.AnsText.Location = new System.Drawing.Point(43, 299);
+            this.AnsText.Name = "AnsText";
+            this.AnsText.Size = new System.Drawing.Size(716, 31);
+            this.AnsText.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("MS UI Gothic", 18F);
+            this.button1.Location = new System.Drawing.Point(269, 382);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 46);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "問２改";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.label3.Location = new System.Drawing.Point(41, 382);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "変換前：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.label4.Location = new System.Drawing.Point(41, 418);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "変換後：";
+            // 
+            // BeforeText
+            // 
+            this.BeforeText.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.BeforeText.Location = new System.Drawing.Point(126, 379);
+            this.BeforeText.Name = "BeforeText";
+            this.BeforeText.Size = new System.Drawing.Size(100, 27);
+            this.BeforeText.TabIndex = 7;
+            // 
+            // AfterText
+            // 
+            this.AfterText.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.AfterText.Location = new System.Drawing.Point(126, 415);
+            this.AfterText.Name = "AfterText";
+            this.AfterText.Size = new System.Drawing.Size(100, 27);
+            this.AfterText.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(882, 480);
+            this.Controls.Add(this.AfterText);
+            this.Controls.Add(this.BeforeText);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.AnsText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ButtonQ4);
+            this.Controls.Add(this.ButtonQ5);
+            this.Controls.Add(this.ButtonQ3);
+            this.Controls.Add(this.ButtonQ2);
+            this.Controls.Add(this.ButtonQ1);
+            this.Controls.Add(this.inputStrText);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox inputStrText;
+        private System.Windows.Forms.Button ButtonQ1;
+        private System.Windows.Forms.Button ButtonQ2;
+        private System.Windows.Forms.Button ButtonQ3;
+        private System.Windows.Forms.Button ButtonQ5;
+        private System.Windows.Forms.Button ButtonQ4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox AnsText;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox BeforeText;
+        private System.Windows.Forms.TextBox AfterText;
+    }
+}
+
---
+++ Chapter05/Exercise3/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise3 {
+    public partial class Form1 : Form {
+        
+        //コンストラクタ
+        public Form1() {
+            InitializeComponent();
+        }
+        //フォームロードタイニングに１回だけ実行される　※コンストラクタの次に実行される
+        private void Form1_Load(object sender, EventArgs e) {
+            inputStrText.Text = "Jackdaws love my big sphinx of quartz";
+        }
+        //問１
+        private void ButtonQ1_Click(object sender, EventArgs e) {
+            AnsText.ResetText();
+            var ans =  inputStrText.Text.Count(n => n == ' ');
+            AnsText.Text = ans.ToString();
+        }
+        //問２
+        private void ButtonQ2_Click(object sender, EventArgs e) {
+            AnsText.ResetText();
+            var target = inputStrText.Text;
+            var ans = target.Replace("big", "small");
+            AnsText.Text = ans;
+        }
+        //問３
+        private void ButtonQ3_Click(object sender, EventArgs e) {
+            AnsText.ResetText();
+            AnsText.Text = inputStrText.Text.Split(' ').Count().ToString();
+        }
+        //問４
+        private void ButtonQ4_Click(object sender, EventArgs e) {
+            AnsText.ResetText();
+            var words = inputStrText.Text.Split(' ').Where(s => s.Length <= 4);
+            foreach(var n in words) {
+                AnsText.Text += n + ",";
+            }
+        }
+        //問５
+        private void ButtonQ5_Click(object sender, EventArgs e) {
+            AnsText.ResetText();
+            var words = inputStrText.Text.Split(' ');
+            var sb = new StringBuilder();
+            foreach(var n in words) {
+                sb.Append(n);
+            }
+            var text = sb.ToString();
+            AnsText.Text = text;
+
+        }
+        //問２改
+        private void button1_Click(object sender, EventArgs e) {
+            var target = inputStrText.Text;
+            var ans = target.Replace(BeforeText.Text,AfterText.Text);
+            AnsText.Text = ans;
+        }
+    }
+}
