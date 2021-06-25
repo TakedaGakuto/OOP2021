715b3953b601ff4ab2ecdf9ee7e4cd1ed183e712
GakutoTakeda <32042@ojs.ac.jp>
2021-06-25 15:55:21 +0900

    P165まで終了

---
+++ Chapter06/Section01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+    class Program {
+        static void Main(string[] args) {
+            var books = Books.GetBooks();
+
+            Console.WriteLine("本の平均価格は" + books.Average(x => x.Price).ToString("#,0") + "円");
+            Console.WriteLine("本のページ数の合計は" + books.Sum(x => x.Pages)+ "ページ");
+            Console.WriteLine("最も高価な本は、" + books.Max(x => x.Price).ToString("#,0") + "円");
+            Console.WriteLine("最も安価な本は、" + books.Min(x => x.Price).ToString("#,0") + "円");
+            Console.WriteLine("500円以上の本は、" + books.Where(x => x.Price >= 500).Count()+"冊");
+        }
+    }
+}
