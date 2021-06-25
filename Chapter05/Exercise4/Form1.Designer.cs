143c5e4a5f3b317c184518473cdd9305e6dc45a2
GakutoTakeda <32042@ojs.ac.jp>
2021-06-25 14:42:33 +0900

    練習問題5終了

---
+++ Chapter05/Exercise4/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿
+namespace Exercise4 {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.inputStrText = new System.Windows.Forms.TextBox();
+            this.RunButton = new System.Windows.Forms.Button();
+            this.AnsText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 20F);
+            this.label1.Location = new System.Drawing.Point(13, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "問題5.4";
+            // 
+            // inputStrText
+            // 
+            this.inputStrText.Font = new System.Drawing.Font("MS UI Gothic", 18F);
+            this.inputStrText.Location = new System.Drawing.Point(18, 34);
+            this.inputStrText.Name = "inputStrText";
+            this.inputStrText.Size = new System.Drawing.Size(666, 31);
+            this.inputStrText.TabIndex = 1;
+            // 
+            // RunButton
+            // 
+            this.RunButton.Font = new System.Drawing.Font("MS UI Gothic", 18F);
+            this.RunButton.Location = new System.Drawing.Point(265, 129);
+            this.RunButton.Name = "RunButton";
+            this.RunButton.Size = new System.Drawing.Size(95, 47);
+            this.RunButton.TabIndex = 2;
+            this.RunButton.Text = "実行";
+            this.RunButton.UseVisualStyleBackColor = true;
+            this.RunButton.Click += new System.EventHandler(this.RunButton_Click);
+            // 
+            // AnsText
+            // 
+            this.AnsText.Font = new System.Drawing.Font("MS UI Gothic", 18F);
+            this.AnsText.Location = new System.Drawing.Point(39, 219);
+            this.AnsText.Multiline = true;
+            this.AnsText.Name = "AnsText";
+            this.AnsText.Size = new System.Drawing.Size(609, 162);
+            this.AnsText.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.AnsText);
+            this.Controls.Add(this.RunButton);
+            this.Controls.Add(this.inputStrText);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox inputStrText;
+        private System.Windows.Forms.Button RunButton;
+        private System.Windows.Forms.TextBox AnsText;
+    }
+}
+
---
+++ Chapter05/Exercise4/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise4 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            inputStrText.Text = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
+        }
+
+        private void RunButton_Click(object sender, EventArgs e) {
+            foreach(var pair in inputStrText.Text.Split(';')) {
+                var array = pair.Split('=').ToArray();
+                AnsText.Text += ToJapanese(array[0]) + ":" + array[1]+"\r\n";
+            }
+        }
+
+        private string ToJapanese(string Key) {
+            switch (Key) {
+                case "Novelist":
+                    return "作者　";
+                case "BestWork":
+                    return "代表作";
+                case "Born":
+                    return "誕生年";
+            }
+            
+            throw new ArgumentException("引数が正しくありません。");
+        }
+    }
+}
