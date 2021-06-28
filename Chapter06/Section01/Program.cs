35d9cd2b7d93a0056c246ceeaaad932f62fd4a15
GakutoTakeda <32042@ojs.ac.jp>
2021-06-28 14:57:31 +0900

    終了

--- Chapter06/Section01/Program.cs
+++ Chapter06/Section01/Program.cs
@@ -9,11 +9,28 @@ namespace Section01 {
         static void Main(string[] args) {
             var books = Books.GetBooks();
 
-            Console.WriteLine("本の平均価格は" + books.Average(x => x.Price).ToString("#,0") + "円");
+            /*Console.WriteLine("本の平均価格は" + books.Average(x => x.Price).ToString("#,0") + "円");
             Console.WriteLine("本のページ数の合計は" + books.Sum(x => x.Pages)+ "ページ");
             Console.WriteLine("最も高価な本は、" + books.Max(x => x.Price).ToString("#,0") + "円");
             Console.WriteLine("最も安価な本は、" + books.Min(x => x.Price).ToString("#,0") + "円");
-            Console.WriteLine("500円以上の本は、" + books.Where(x => 500 <= x.Price).Count()+"冊");
+            Console.WriteLine("500円以上の本は、" + books.Count(x => x.Price >= 500) +"冊");
+            Console.WriteLine("「物語」が含まれている本は、" + books.Count(x => x.Title.Contains("物語")) +"冊");
+
+            var titles = books.Where(x => x.Title.Contains("物語"));
+            foreach(var book in titles) {
+                Console.WriteLine(book.Title);
+            }
+            int i = 1;
+            Console.WriteLine("高額な本ベスト３は、");
+            var best = books.OrderByDescending(x => x.Price).Take(3);
+            foreach(var best3 in best) {
+                Console.WriteLine("{0}位　{1} {2}円",i++,best3.Title,best3.Price);
+            }*/
+            //P178
+            var titles = books.Select(x => x.Title);
+            foreach(var item in titles) {
+                Console.WriteLine(item);
+            }
         }
     }
 }
