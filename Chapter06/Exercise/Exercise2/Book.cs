eb24264eb38504a605f85e396f5ec206c8f554a4
GakutoTakeda <32042@ojs.ac.jp>
2021-06-28 15:57:19 +0900

    問題6.2途中

---
+++ Chapter06/Exercise/Exercise2/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// List 6-1
+
+namespace Exercise2 {
+    class Book {
+        public string Title { get; set; }
+        public int Price { get; set; }
+        public int Pages { get; set; }
+    }
+}
