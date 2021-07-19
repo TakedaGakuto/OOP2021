193ac231bd0ee011f5e669e063959783722af595
岳人 竹田 <32042@ojs.ac.jp>
2021-07-19 14:57:03 +0900

    問題9.2まで終了

---
+++ Chapter09/Exercise/Exercise2/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace Exercise2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btOpen = new System.Windows.Forms.Button();
+            this.ofdOpenFile = new System.Windows.Forms.OpenFileDialog();
+            this.tbOutPut = new System.Windows.Forms.TextBox();
+            this.sfdSaveFile = new System.Windows.Forms.SaveFileDialog();
+            this.btSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btOpen
+            // 
+            this.btOpen.Font = new System.Drawing.Font("MS UI Gothic", 18F);
+            this.btOpen.Location = new System.Drawing.Point(42, 57);
+            this.btOpen.Name = "btOpen";
+            this.btOpen.Size = new System.Drawing.Size(76, 35);
+            this.btOpen.TabIndex = 0;
+            this.btOpen.Text = "開く...";
+            this.btOpen.UseVisualStyleBackColor = true;
+            this.btOpen.Click += new System.EventHandler(this.btOpen_Click);
+            // 
+            // ofdOpenFile
+            // 
+            this.ofdOpenFile.FileName = "openFileDialog1";
+            // 
+            // tbOutPut
+            // 
+            this.tbOutPut.Location = new System.Drawing.Point(42, 99);
+            this.tbOutPut.Multiline = true;
+            this.tbOutPut.Name = "tbOutPut";
+            this.tbOutPut.Size = new System.Drawing.Size(721, 339);
+            this.tbOutPut.TabIndex = 1;
+            // 
+            // btSave
+            // 
+            this.btSave.Font = new System.Drawing.Font("MS UI Gothic", 18F);
+            this.btSave.Location = new System.Drawing.Point(140, 57);
+            this.btSave.Name = "btSave";
+            this.btSave.Size = new System.Drawing.Size(73, 34);
+            this.btSave.TabIndex = 2;
+            this.btSave.Text = "保存";
+            this.btSave.UseVisualStyleBackColor = true;
+            this.btSave.Click += new System.EventHandler(this.btSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btSave);
+            this.Controls.Add(this.tbOutPut);
+            this.Controls.Add(this.btOpen);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btOpen;
+        private System.Windows.Forms.OpenFileDialog ofdOpenFile;
+        private System.Windows.Forms.TextBox tbOutPut;
+        private System.Windows.Forms.SaveFileDialog sfdSaveFile;
+        private System.Windows.Forms.Button btSave;
+    }
+}
+
---
+++ Chapter09/Exercise/Exercise2/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Exercise2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btOpen_Click(object sender, EventArgs e)
+        {
+            int count = 0;
+            if (ofdOpenFile.ShowDialog() == DialogResult.OK)
+            {
+                using (var reader = new StreamReader(ofdOpenFile.FileName, Encoding.UTF8))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine(); //1行読み込み
+                        count++;
+                        tbOutPut.Text += count + ":"+ line+"\r\n";
+                    }
+                }
+            }
+        }
+        private void btSave_Click(object sender, EventArgs e)
+        {
+            if (sfdSaveFile.ShowDialog() == DialogResult.OK) {
+                using (var writer = new StreamWriter(sfdSaveFile.FileName))
+                {
+                    writer.WriteLine(tbOutPut.Text);
+                }
+            }
+        }
+    }
+}
