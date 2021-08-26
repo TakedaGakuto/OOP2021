937ff3577bdabec2a8d2a5ae61bfc8669aac17a7
岳人 竹田 <32042@ojs.ac.jp>
2021-08-26 13:33:20 +0900

    Sample0823終了

---
+++ DistanceConverter/Sample0823/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace Sample0823
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bt = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbNumber = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbJyou = new System.Windows.Forms.TextBox();
+            this.tbResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // bt
+            // 
+            this.bt.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.bt.Location = new System.Drawing.Point(340, 106);
+            this.bt.Name = "bt";
+            this.bt.Size = new System.Drawing.Size(85, 47);
+            this.bt.TabIndex = 0;
+            this.bt.Text = "結果";
+            this.bt.UseVisualStyleBackColor = true;
+            this.bt.Click += new System.EventHandler(this.bt_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 16F);
+            this.label1.Location = new System.Drawing.Point(149, 119);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 27);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "の";
+            // 
+            // tbNumber
+            // 
+            this.tbNumber.Font = new System.Drawing.Font("MS UI Gothic", 16F);
+            this.tbNumber.Location = new System.Drawing.Point(43, 112);
+            this.tbNumber.Name = "tbNumber";
+            this.tbNumber.Size = new System.Drawing.Size(100, 34);
+            this.tbNumber.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 16F);
+            this.label2.Location = new System.Drawing.Point(295, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 27);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "乗";
+            // 
+            // tbJyou
+            // 
+            this.tbJyou.Font = new System.Drawing.Font("MS UI Gothic", 16F);
+            this.tbJyou.Location = new System.Drawing.Point(189, 112);
+            this.tbJyou.Name = "tbJyou";
+            this.tbJyou.Size = new System.Drawing.Size(100, 34);
+            this.tbJyou.TabIndex = 2;
+            // 
+            // tbResult
+            // 
+            this.tbResult.Font = new System.Drawing.Font("MS UI Gothic", 16F);
+            this.tbResult.Location = new System.Drawing.Point(448, 112);
+            this.tbResult.Name = "tbResult";
+            this.tbResult.Size = new System.Drawing.Size(100, 34);
+            this.tbResult.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbResult);
+            this.Controls.Add(this.tbJyou);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbNumber);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbNumber;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbJyou;
+        private System.Windows.Forms.TextBox tbResult;
+    }
+}
+
---
+++ DistanceConverter/Sample0823/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sample0823
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void bt_Click(object sender, EventArgs e)
+        {
+
+            int ans = 1;
+            for(int i = 0; i < int.Parse(tbJyou.Text); i++)
+            {
+                ans = ans * int.Parse(tbNumber.Text);
+            }
+            tbResult.Text = ans.ToString() ;
+
+            //tbResult.Text = Math.Pow(int.Parse(tbNumber.Text), int.Parse(tbJyou.Text)).ToString();
+        }
+    }
+}
