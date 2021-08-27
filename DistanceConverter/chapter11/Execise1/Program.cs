cf2a4ef4a38997569c080bd4baf004eb45acf3a5
岳人 竹田 <32042@ojs.ac.jp>
2021-08-27 15:20:57 +0900

    問題11.1.1

---
+++ DistanceConverter/chapter11/Execise1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Execise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var file = "Sample.xml";
+            Execise1_1(file);
+            Console.WriteLine("----------");
+            Execise1_2(file);
+            Console.WriteLine("----------");
+            Execise1_3(file);
+            Console.WriteLine("----------");
+        }
+
+        private static void Execise1_1(string file)
+        {
+            var xdoc = XDocument.Load(file);
+            var xelements = xdoc.Root.Elements();
+            foreach(var xlist in xelements)
+            {
+                XElement xname = xlist.Element("name");
+                XElement xmember = xlist.Element("teammembers");
+                Console.WriteLine("競技名:{0}チームメンバー数:{1}",xname.Value,xmember.Value);
+            }
+        }
+        private static void Execise1_2(string file)
+        {
+            var xdoc = XDocument.Load(file);
+            var xelements = xdoc.Root.Elements()
+                                     .OrderBy(x => (x.Element("firstplayed")));
+            foreach(var xlist in xelements)
+            {
+                var xname = xlist.Element("name");
+                XAttribute xkanji = xname.Attribute("kanji");
+                Console.WriteLine(xkanji?.Value);
+            }
+        }
+
+        private static void Execise1_3(string file)
+        {
+     
+        }
+    }
+}
