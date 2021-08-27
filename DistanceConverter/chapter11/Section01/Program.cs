db266274958320055c740805bfaf742c2e874725
岳人 竹田 <32042@ojs.ac.jp>
2021-08-27 14:06:10 +0900

    リスト11.10

--- DistanceConverter/chapter11/Section01/Program.cs
+++ DistanceConverter/chapter11/Section01/Program.cs
@@ -11,22 +11,36 @@ namespace Section01
     {
         static void Main(string[] args)
         {
-            var xdoc = XDocument.Load("novelists.xml");
+
             /*var xelements = xdoc.Root.Elements()
                 .Where(x=>((DateTime)x.Element("birth")).Year >= 1900);
             var xelements = xdoc.Root.Elements()
                 .OrderBy(x => (string)(x.Element("birth")));*/
-            var novelists = xdoc.Root.Elements()
-                                .Select(x => new
-                                {
-                                    Name = (string)x.Element("name"),
-                                    Birth = (DateTime)x.Element("birth"),
-                                    Death = (DateTime)x.Element("death")
-                                });
+            
             foreach (var novelist in novelists)
             {
                 Console.WriteLine("{0} ({1}-{2})",novelist.Name,novelist.Birth.Year,novelist.Death.Year);
             }
         }
+
+        public IEnumerable<Novelist> ReadNovelists()
+        {
+            var xdoc = XDocument.Load("novelists.xml");
+            var novelists = xdoc.Root.Elements()
+                                .Select(x => new Novelist
+                                {
+                                    Name = (string)x.Element("name"),
+                                    KanaName = (string)(x.Element("name").Attribute("kane")),
+                                    Birth = (DateTime)x.Element("birth"),
+                                    Death = (DateTime)x.Element("death"),
+                                    Masterpieces = x.Element("masterpieces")
+                                                    .Elements("title")
+                                                    .Select(title => title.Value)
+                                                    .ToArray()
+                                });
+            return novelists.ToArray();
+        }
+
+
     }
 }
