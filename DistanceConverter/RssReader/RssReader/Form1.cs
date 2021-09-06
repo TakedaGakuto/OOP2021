91788a3d9c3bc570a3681e0cc077bbf0a37b50e2
岳人 竹田 <32042@ojs.ac.jp>
2021-09-06 15:59:30 +0900

    途中

---
+++ DistanceConverter/RssReader/RssReader/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace RssReader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btRead_Click(object sender, EventArgs e)
+        {
+            GetWebsite(tbUrl.Text);
+        }
+
+        private void GetWebsite(string text)
+        {
+            using (var wc = new WebClient())
+            {
+                wc.Headers.Add("Content-type", "charset=UTF-8");
+                var stream = wc.OpenRead(text);
+                XDocument xdoc = XDocument.Load(stream);
+                var nodes = xdoc.Root.Descendants("title");
+                foreach (var node in nodes)
+                {
+                    lbTitles.Items.Add(node);
+                }
+            }
+        }
+    }
+}
