16f483177fcc44f1bf0b3ed5d4695887208127c4
岳人 竹田 <32042@ojs.ac.jp>
2021-11-17 15:53:57 +0900

    途中

--- DistanceConverter/WPF/NumberGame/MainWindow.xaml.cs
+++ DistanceConverter/WPF/NumberGame/MainWindow.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -21,36 +23,66 @@ namespace NumberGame
     /// </summary>
     public partial class MainWindow : Window
     {
-        int num=0;
-        string Message;
+        int num = 0;
+        private const int Rows = 5;
+        private const int Columns = 5;
+        Stopwatch stopwatch = new Stopwatch();
+
         public MainWindow()
         {
             InitializeComponent();
             Random random = new Random();
             num = random.Next(1, 25);
-            
-
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             //値より下
-            
             if(num > int.Parse(((Button)sender).Content.ToString()))
             {
+                ((Button)sender).Background = Brushes.Gray;
+                //((Button)sender).IsEnabled = false;
                 label.Content = "もう少し高い値です。";
             }
             //同値
             if(num == int.Parse(((Button)sender).Content.ToString()))
             {
+                ((Button)sender).Background = Brushes.Red;
                 label.Content = "正解です。";
             }
             //値より上
             if(num < int.Parse(((Button)sender).Content.ToString()))
             {
+                ((Button)sender).Background = Brushes.Gray;
                 label.Content = "もう少し低い値です。";
             }
-            
         }
+        /*private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //行
+            for (int i = 0; i < Rows; i++)
+            {
+                grid.RowDefinitions.Add(new RowDefinition());
+            }
+            //列
+            for (int i = 0; i < Columns; i++)
+            {
+                grid.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+            for (int i = 0; i < Rows; i++)
+            {
+                for (int j = 0; j < Columns; i++)
+                {
+                    var bt = new Button();
+                    bt.Width = Main.Width/Columns;
+                    bt.Height = Main.Height/Rows;
+                    bt.Content = 1;
+                    bt.FontSize = 20;
+                    bt.Click += Button_Click;
+                    Grid.SetRow(bt, i);
+                    Grid.SetColumn(bt, j);
+                }
+            }
+        }*/
     }
 }
